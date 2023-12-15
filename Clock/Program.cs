2cc7b4fa9bee7cd9119bc17fab8c5c1182250624
StefanGrace <stefangrace@hotmail.co.nz>
2023-12-15 22:32:34 +1300

    Add files via upload

--- Clock/Program.cs
+++ Clock/Program.cs
@@ -13,7 +13,7 @@ namespace Clock
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ClockForm());
         }
     }
 }
